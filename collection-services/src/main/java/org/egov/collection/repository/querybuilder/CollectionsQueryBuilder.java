35c681868cff50e83be52a2dd5cf0b6cf8bddbfc
Vishal Mahuli <vishal.mahuli@tarento.com>
2019-09-25 11:44:22 +0530

    query changed

--- collection-services/src/main/java/org/egov/collection/repository/querybuilder/CollectionsQueryBuilder.java
+++ collection-services/src/main/java/org/egov/collection/repository/querybuilder/CollectionsQueryBuilder.java
@@ -92,7 +92,7 @@ public class CollectionsQueryBuilder {
     		"as ins_lastmodifiedby ,  ins.lastmodifieddate as ins_lastmodifieddate , ins.tenantid as ins_tenantid ,  ins.instrumentDate as ins_instrumentDate, "
     		+ "ins.instrumentNumber as ins_instrumentNumber \n" + 
     		"\n" + 
-    		"from egcl_receiptheader_v1 rh LEFT JOIN egcl_receiptdetails_v1 rd ON rh.id=rd.receiptheader INNER JOIN egcl_receiptinstrument_v1 recins ON rh.id=recins.receiptheader "
+    		"from egcl_receiptheader_v1 rh LEFT JOIN egcl_receiptdetails_v1 rd ON rh.id=rd.receiptheader LEFT JOIN egcl_receiptinstrument_v1 recins ON rh.id=recins.receiptheader "
     		+ "LEFT JOIN egcl_instrumentheader_v1 ins ON recins.instrumentheader=ins.id ";
 
     private static final String PAGINATION_WRAPPER = "SELECT * FROM " +
