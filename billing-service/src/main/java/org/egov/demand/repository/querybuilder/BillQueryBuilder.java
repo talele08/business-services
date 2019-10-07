77d9941959402144fcc8600e890ad0f4bbf8639d
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2019-10-07 13:29:59 +0530

    query updated for v1 and v2 commonly

--- billing-service/src/main/java/org/egov/demand/repository/querybuilder/BillQueryBuilder.java
+++ billing-service/src/main/java/org/egov/demand/repository/querybuilder/BillQueryBuilder.java
@@ -15,8 +15,8 @@ public class BillQueryBuilder {
 	private ApplicationProperties applicationProperties;
 	
 	public static final String INSERT_BILL_QUERY = "INSERT into egbs_bill_v1 "
-			+"(id, tenantid, payername, payeraddress, payeremail, isactive, iscancelled, createdby, createddate, lastmodifiedby, lastmodifieddate, mobilenumber)"
-			+"values(?,?,?,?,?,?,?,?,?,?,?,?)";
+			+"(id, tenantid, payername, payeraddress, payeremail, isactive, iscancelled, createdby, createddate, lastmodifiedby, lastmodifieddate, mobilenumber, status, additionaldetails)"
+			+"values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 	
 	public static final String INSERT_BILLDETAILS_QUERY = "INSERT into egbs_billdetail_v1 "
 			+"(id, tenantid, billid, demandid, fromperiod, toperiod, businessservice, billno, billdate, consumercode, consumertype, billdescription, displaymessage, "
@@ -31,7 +31,7 @@ public class BillQueryBuilder {
 	
 	public static final String BILL_BASE_QUERY = "SELECT b.id AS b_id,b.mobilenumber, b.tenantid AS b_tenantid,"
 			+ " b.payername AS b_payername, b.payeraddress AS b_payeraddress, b.payeremail AS b_payeremail,"
-			+ " b.isactive AS b_isactive, b.iscancelled AS b_iscancelled, b.createdby AS b_createdby,"
+			+ " b.isactive AS b_isactive, b.iscancelled AS b_iscancelled, b.createdby AS b_createdby, b.status as b_status,"
 			+ " b.createddate AS b_createddate, b.lastmodifiedby AS b_lastmodifiedby, b.lastmodifieddate AS b_lastmodifieddate,"
 			+ " bd.id AS bd_id, bd.billid AS bd_billid, bd.tenantid AS bd_tenantid,bd.businessservice AS bd_businessservice,"
 			+ " bd.demandid,bd.fromperiod,bd.toperiod,"
@@ -42,7 +42,8 @@ public class BillQueryBuilder {
 			+ " ad.id AS ad_id, ad.tenantid AS ad_tenantid, ad.billdetail AS ad_billdetail, ad.glcode AS ad_glcode,"
 			+ " ad.orderno AS ad_orderno, ad.accountdescription AS ad_accountdescription,"
 			+ " ad.amount AS ad_amount, ad.adjustedamount AS ad_adjustedamount, ad.taxheadcode AS ad_taxheadcode, ad.demanddetailid,"
-			+ " ad.isactualdemand AS ad_isactualdemand, ad.purpose AS ad_purpose"
+			+ " ad.isactualdemand AS ad_isactualdemand, ad.purpose AS ad_purpose,"
+			+ " b.additionaldetails as b_additionaldetails,  bd.additionaldetails as bd_additionaldetails,  ad.additionaldetails as ad_additionaldetails"
 			+ " FROM egbs_bill_v1 b"
 			+ " LEFT OUTER JOIN egbs_billdetail_v1 bd ON b.id = bd.billid AND b.tenantid = bd.tenantid"
 			+ " LEFT OUTER JOIN egbs_billaccountdetail_v1 ad ON bd.id = ad.billdetail AND bd.tenantid = ad.tenantid"
